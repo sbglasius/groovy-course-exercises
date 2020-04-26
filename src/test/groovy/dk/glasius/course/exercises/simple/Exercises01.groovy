a8ca3f94ef66ea07b4a7d47c7009cd5e7af4058b
Søren Berg Glasius <soeren@glasius.dk>
2020-04-26 12:22:08 +0200

    String exercises

---
+++ src/test/groovy/dk/glasius/course/exercises/simple/Exercises01.groovy
@@ -0,0 +1,109 @@
+package dk.glasius.course.exercises.simple
+
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * See http://groovy-lang.org/syntax.html#all-strings
+ */
+class Exercises01 extends Specification {
+
+    @Unroll
+    def 'Types of String classes'() {
+        expect:
+            string.getClass().is(clazz)
+        where:
+            string     || clazz
+            'a'        || Object
+            "a"        || Object
+            '${1}'     || Object
+            "${1}"     || Object
+            '''a'''    || Object
+            '''${1}''' || Object
+            """a"""    || Object
+            """${1}""" || Object
+            /${1}/     || Object
+            /$1/       || Object
+            $/${1}/$   || Object
+            $/$$1/$    || Object
+    }
+
+    def 'How to handle chars'() {
+        given: "Fix this statement"
+            def c = 'A'
+
+        expect: "So that this does not fail"
+            c instanceof Character
+    }
+
+    def 'Testing GStrings'() {
+        given: 'Two variables'
+            def number = 42
+            def what = 'life, the Universe and everything'
+        and: 'A GString'
+            def gstring = ''
+
+        expect: 'the answer to be'
+            gstring == '42 is the answer to life, the Universe and everything'
+
+        when: 'changing the number'
+            number = 45
+
+        then: 'what will the right value for the gstring?'
+            gstring == '?'
+    }
+
+    def 'Testing GStrings lazy evaluation'() {
+        given: 'A variable'
+            def a = 1
+        and: 'A GString'
+            def gstring = ''
+
+        expect: 'the evaluation of the GString should be'
+            gstring == 'a is 1'
+
+        when: 'changing a'
+            a = 2
+
+        then: 'without changing the gstring, the evaluation of the GString should be'
+            gstring == 'a is 2'
+    }
+
+    def 'repeat a string'() {
+        expect: 'a Groovy way to repeat a string'
+            '*-*'  == '*-**-**-**-**-**-**-**-**-**-*'
+    }
+
+    def 'remove part of a string'() {
+        expect: 'an intuitive way to remove part of a string'
+            'This is NOT Groovy'  == 'This is Groovy'
+    }
+
+    @Unroll
+    def 'align a string'() {
+        expect: 'a Groovy way to pad a string'
+            "$number" == output
+
+        where: 'data is taken from this table'
+            number || output
+            1      || '    1'
+            10     || '   10'
+            100    || '  100'
+            1000   || ' 1000'
+            10000  || '10000'
+    }
+
+    def 'an easy way to make a string with multiple lines indented right'() {
+        expect: 'a multiline string to produce the right output'
+            ''.stripIndent() == '<x>\n    <y>Z</y>\n</x>'
+    }
+
+    def 'Hashing strings'() {
+        expect: 'This is true'
+            "Value: ${1}" == 'Value: 1'
+        and: 'This is not true, explain why'
+            "Value: ${1}".hashCode() != 'Value: 1'.hashCode()
+    }
+
+}
